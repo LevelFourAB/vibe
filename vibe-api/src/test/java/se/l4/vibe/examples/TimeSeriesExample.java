2aa5e38a6ae35352e591768188f0a870c66db6fc
Andreas Holstenson <a@holstenson.se>
2011-11-28 16:23:01 +0100

    Added a simple example

---
+++ vibe-api/src/test/java/se/l4/vibe/examples/TimeSeriesExample.java
@@ -0,0 +1,32 @@
+package se.l4.vibe.examples;
+
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+import se.l4.vibe.Vibe;
+import se.l4.vibe.VibeBuilder;
+import se.l4.vibe.probes.RuntimeProbes;
+
+/**
+ * A simple example that will log the current CPU usage every 5 seconds.
+ * 
+ * @author Andreas Holstenson
+ *
+ */
+public class TimeSeriesExample
+{
+	public static void main(String[] args)
+		throws IOException
+	{
+		Vibe vibe = new VibeBuilder()
+			.setSamplingInterval(4, TimeUnit.SECONDS)
+			.build();
+		
+		vibe.timeSeries(RuntimeProbes.getCpuUsage())
+			.at("sys/cpu")
+			.export();
+		
+		System.out.println("Press enter to exit");
+		System.in.read();
+	}
+}
