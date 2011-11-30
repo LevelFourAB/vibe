c751794c0a5325001020be1a92cec8c20d97f53a
Andreas Holstenson <a@holstenson.se>
2011-11-30 15:08:22 +0100

    Triggers for minimum and maximum values

--- vibe-api/src/main/java/se/l4/vibe/trigger/Triggers.java
+++ vibe-api/src/main/java/se/l4/vibe/trigger/Triggers.java
@@ -4,6 +4,7 @@ import java.util.concurrent.TimeUnit;
 
 import se.l4.vibe.probes.Average;
 import se.l4.vibe.probes.Probe;
+import se.l4.vibe.probes.Range;
 import se.l4.vibe.probes.TimeSeries;
 
 /**
@@ -64,33 +65,81 @@ public class Triggers
 			{
 				return "average over " + duration + " " + toReadable(unit, duration);
 			}
-
-			private String toReadable(TimeUnit unit, long duration)
+		};
+	}
+	
+	/**
+	 * Calculate the minimum value over a specific duration.
+	 * 
+	 * @param duration
+	 * @param unit
+	 * @return
+	 */
+	public static <T extends Number> Trigger<T, Double> minimum(final long duration, final TimeUnit unit)
+	{
+		return new Trigger<T, Double>()
+		{
+			@Override
+			public Probe<Double> forTimeSeries(TimeSeries<T> series)
 			{
-				boolean one = duration == 1;
-				
-				switch(unit)
-				{
-					case DAYS:
-						return one ? "day" : "days";
-					case HOURS:
-						return one ? "hour" : "hours";
-					case MICROSECONDS:
-						return one ? "microsecond" : "microseconds";
-					case MILLISECONDS:
-						return one ? "millisecond" : "milliseconds";
-					case MINUTES:
-						return one ? "minute" : "minutes";
-					case NANOSECONDS:
-						return one ? "nanosecond" : "nanoseconds";
-					case SECONDS:
-						return one ? "second" : "seconds";
-				}
-				
-				return unit.toString();
+				return Range.min(series, duration, unit);
+			}
+			
+			@Override
+			public String toString()
+			{
+				return "minimum over " + duration + " " + toReadable(unit, duration);
 			}
 		};
 	}
 	
+	/**
+	 * Calculate the maximum value over a specific duration.
+	 * 
+	 * @param duration
+	 * @param unit
+	 * @return
+	 */
+	public static <T extends Number> Trigger<T, Double> maximum(final long duration, final TimeUnit unit)
+	{
+		return new Trigger<T, Double>()
+		{
+			@Override
+			public Probe<Double> forTimeSeries(TimeSeries<T> series)
+			{
+				return Range.max(series, duration, unit);
+			}
+			
+			@Override
+			public String toString()
+			{
+				return "maximum over " + duration + " " + toReadable(unit, duration);
+			}
+		};
+	}
 	
+	private static String toReadable(TimeUnit unit, long duration)
+	{
+		boolean one = duration == 1;
+		
+		switch(unit)
+		{
+			case DAYS:
+				return one ? "day" : "days";
+			case HOURS:
+				return one ? "hour" : "hours";
+			case MICROSECONDS:
+				return one ? "microsecond" : "microseconds";
+			case MILLISECONDS:
+				return one ? "millisecond" : "milliseconds";
+			case MINUTES:
+				return one ? "minute" : "minutes";
+			case NANOSECONDS:
+				return one ? "nanosecond" : "nanoseconds";
+			case SECONDS:
+				return one ? "second" : "seconds";
+		}
+		
+		return unit.toString();
+	}
 }
