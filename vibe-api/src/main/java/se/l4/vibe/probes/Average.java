d16fb0e7934c19066ce7ce78ec66ff2d25342b2e
Andreas Holstenson <a@holstenson.se>
2011-12-04 21:36:19 +0100

    Hid AverageProbe

--- vibe-api/src/main/java/se/l4/vibe/probes/Average.java
+++ vibe-api/src/main/java/se/l4/vibe/probes/Average.java
@@ -99,4 +99,40 @@ public class Average
 			return totalSum / totalEntries;
 		}
 	}
+	
+	/**
+	 * Probe that calculates the average of another probe.
+	 * 
+	 * @author Andreas Holstenson
+	 *
+	 * @param <T>
+	 */
+	private static class AveragingProbe<T extends Number>
+		extends AbstractSampledProbe<Double>
+	{
+		private long accumulated;
+		private long samples;
+		private final SampledProbe<T> probe;
+
+		public AveragingProbe(SampledProbe<T> probe)
+		{
+			this.probe = probe;
+		}
+
+		@Override
+		public Double peek()
+		{
+			long diff = probe.peek().longValue();
+			return (accumulated + diff) / (samples + 1.0);
+		}
+
+		@Override
+		protected Double sample0()
+		{
+			accumulated += probe.read().longValue(); 
+			samples++;
+			return accumulated / (double) samples;
+		}
+
+	}
 }
