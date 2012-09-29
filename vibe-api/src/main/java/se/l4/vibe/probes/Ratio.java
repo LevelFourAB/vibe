550095df067a40d64009f65e5b3b144db0f9eded
Andreas Holstenson <a@holstenson.se>
2012-09-29 22:58:44 +0200

    Improved ratio support

--- vibe-api/src/main/java/se/l4/vibe/probes/Ratio.java
+++ vibe-api/src/main/java/se/l4/vibe/probes/Ratio.java
@@ -7,16 +7,9 @@ package se.l4.vibe.probes;
  *
  */
 public class Ratio
-	extends AbstractSampledProbe<Double>
 {
-	private final SampledProbe<? extends Number> probe1;
-	private final SampledProbe<? extends Number> probe2;
-
-	public Ratio(SampledProbe<? extends Number> probe1,
-			SampledProbe<? extends Number> probe2)
+	private Ratio()
 	{
-		this.probe1 = probe1;
-		this.probe2 = probe2;
 	}
 	
 	/**
@@ -30,11 +23,11 @@ public class Ratio
 	 * @param probe2
 	 * @return
 	 */
-	public static Ratio between(
+	public static SampledProbe<Double> between(
 			SampledProbe<? extends Number> a,
 			SampledProbe<? extends Number> b)
 	{
-		return new Ratio(a, b);
+		return new SampledRatio(a, b);
 	}
 	
 	/**
@@ -48,11 +41,11 @@ public class Ratio
 	 * @param number
 	 * @return
 	 */
-	public static Ratio between1(
+	public static SampledProbe<Double> between(
 			SampledProbe<? extends Number> a,
 			double b)
 	{
-		return new Ratio(a, new ConstantProbe<Double>(b));
+		return new SampledRatio(a, new ConstantProbe<Double>(b));
 	}
 	
 	/**
@@ -66,22 +59,98 @@ public class Ratio
 	 * @param probe2
 	 * @return
 	 */
-	public static Ratio between(
+	public static SampledProbe<Double> between(
 			double a, 
 			SampledProbe<? extends Number> b)
 	{
-		return new Ratio(new ConstantProbe<Double>(a), b);
+		return new SampledRatio(new ConstantProbe<Double>(a), b);
 	}
 	
-	@Override
-	public Double peek()
+	/**
+	 * Create a new ratio between the two probes. 
+	 * 
+	 * @param probe1
+	 * @param probe2
+	 * @return
+	 */
+	public static Probe<Double> between(
+			Probe<? extends Number> a,
+			Probe<? extends Number> b)
 	{
-		return probe1.peek().doubleValue() / probe2.peek().doubleValue();
+		return new NormalRatio(a, b);
 	}
 	
-	@Override
-	protected Double sample0()
+	/**
+	 * Create a ratio between a probe and a static value.
+	 * 
+	 * @param probe1
+	 * @param number
+	 * @return
+	 */
+	public static Probe<Double> between(
+			Probe<? extends Number> a,
+			double b)
 	{
-		return probe1.sample().doubleValue() / probe2.sample().doubleValue();
+		return new NormalRatio(a, new ConstantProbe<Double>(b));
+	}
+	
+	/**
+	 * Create a ratio between a static value and a probe.
+	 * 
+	 * @param number
+	 * @param probe2
+	 * @return
+	 */
+	public static Probe<Double> between(
+			double a, 
+			Probe<? extends Number> b)
+	{
+		return new NormalRatio(new ConstantProbe<Double>(a), b);
+	}
+	
+	private static class SampledRatio
+		extends AbstractSampledProbe<Double>
+	{
+		private final SampledProbe<? extends Number> probe1;
+		private final SampledProbe<? extends Number> probe2;
+
+		public SampledRatio(SampledProbe<? extends Number> probe1,
+				SampledProbe<? extends Number> probe2)
+		{
+			this.probe1 = probe1;
+			this.probe2 = probe2;
+		}
+		
+		@Override
+		public Double peek()
+		{
+			return probe1.peek().doubleValue() / probe2.peek().doubleValue();
+		}
+		
+		@Override
+		protected Double sample0()
+		{
+			return probe1.sample().doubleValue() / probe2.sample().doubleValue();
+		}
+	}
+	
+	private static class NormalRatio
+		implements Probe<Double>
+	{
+		private final Probe<? extends Number> probe1;
+		private final Probe<? extends Number> probe2;
+	
+		public NormalRatio(Probe<? extends Number> probe1,
+				Probe<? extends Number> probe2)
+		{
+			this.probe1 = probe1;
+			this.probe2 = probe2;
+		}
+		
+		@Override
+		public Double read()
+		{
+			return probe1.read().doubleValue() / probe2.read().doubleValue();
+		}
 	}
 }
