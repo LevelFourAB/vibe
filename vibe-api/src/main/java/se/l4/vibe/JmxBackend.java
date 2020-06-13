f1294557961a1f732bb7b8e8c738e6b33e1f8e2a
Andreas Holstenson <a@holstenson.se>
2020-06-13 09:52:08 +0200

    feat(api): Let JMX backend export SampledProbe instances

--- vibe-api/src/main/java/se/l4/vibe/JmxBackend.java
+++ vibe-api/src/main/java/se/l4/vibe/JmxBackend.java
@@ -1,6 +1,7 @@
 package se.l4.vibe;
 
 import java.lang.management.ManagementFactory;
+import java.time.Duration;
 import java.util.Objects;
 
 import javax.management.InstanceAlreadyExistsException;
@@ -15,6 +16,7 @@ import se.l4.vibe.internal.jmx.ExportMBeanBridge;
 import se.l4.vibe.internal.jmx.JmxExport;
 import se.l4.vibe.internal.jmx.ProbeBean;
 import se.l4.vibe.probes.Probe;
+import se.l4.vibe.sampling.SampledProbe;
 import se.l4.vibe.sampling.TimeSampler;
 
 /**
@@ -23,11 +25,18 @@ import se.l4.vibe.sampling.TimeSampler;
 public class JmxBackend
 	implements VibeBackend
 {
+	private final Duration samplingInterval;
 	private final MBeanServer server;
 	private final String root;
 
-	private JmxBackend(String root, MBeanServer server)
+	private JmxBackend(
+		Duration samplingInterval,
+		String root,
+		MBeanServer server
+	)
 	{
+		this.samplingInterval = samplingInterval;
+
 		// Cut of last . if any
 		this.root = root.charAt(root.length() - 1) == '.'
 			? root.substring(0, root.length() - 1)
@@ -137,7 +146,7 @@ public class JmxBackend
 		 * Add a listener - doesn't actually do anything other than to activate
 		 * sampling.
 		 */
-		Handle h1 = series.addListener(sample -> {});
+		Handle h1 = series.start();
 		Handle h2 = export0(path, new ProbeBean(series));
 
 		return () -> {
@@ -150,6 +159,17 @@ public class JmxBackend
 	public Handle export(String path, Probe<?> probe)
 	{
 		return export0(path, new ProbeBean(probe));
+	}
+
+	@Override
+	public Handle export(String path, SampledProbe<?> probe)
+	{
+		return export(
+			path,
+			TimeSampler.forProbe(probe)
+				.withInterval(samplingInterval)
+				.build()
+		);
 	}
 
 	@Override
@@ -169,6 +189,7 @@ public class JmxBackend
 
 	public static class Builder
 	{
+		private Duration samplingInterval;
 		private String name;
 		private MBeanServer server;
 
@@ -176,6 +197,23 @@ public class JmxBackend
 		{
 			name = "vibe";
 			server = ManagementFactory.getPlatformMBeanServer();
+			samplingInterval = Duration.ofSeconds(10);
+		}
+
+		/**
+		 * Set the sampling interval this backend should use for {@link Probe}s
+		 * and {@link SampledProbe}s.
+		 *
+		 * @param interval
+		 *   interval to use
+		 * @return
+		 */
+		public Builder withSamplingInterval(Duration interval)
+		{
+			Objects.requireNonNull(interval, "interval can not be null");
+			this.samplingInterval = interval;
+
+			return this;
 		}
 
 		public Builder withName(String name)
@@ -196,7 +234,7 @@ public class JmxBackend
 
 		public JmxBackend build()
 		{
-			return new JmxBackend(name, server);
+			return new JmxBackend(samplingInterval, name, server);
 		}
 	}
 }
